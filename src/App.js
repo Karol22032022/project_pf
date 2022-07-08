bd52d57939fb6f73c6fd48a848ca871f29e92e27
unknown <karol.nowakowski@poczta.onet.pl>
2022-07-08 18:47:57 +0200

    AppUpdate

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
-import {LogIn} from "./components/login";
-import {CreateAccount} from "./components/createAccount";
-import {Main} from "./components/main";
+import {LogIn} from "./components/Login";
+import {CreateAccount} from "./components/CreateAccount";
+import {Main} from "./components/Main";
 import "./styles/app.scss"
-import React from "react";
-
 
 function App() {
   return (
-   <>
-
-      <Router>
-
-
-          <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/logIn" element={<LogIn />} />
-          <Route path="/createAccount" element={<CreateAccount />} />
-              <Route path="what_about" element={<LogIn />} />
-              <Route path="/about_us" element={<CreateAccount />} />
-              <Route path="/organization" element={<CreateAccount />} />
-              <Route path="/contact" element={<CreateAccount />} />
-              <Route path="/give_things" element={<CreateAccount />} />
-              <Route path="/organize_action" element={<CreateAccount />} />
-          </Routes>
-
-      </Router>
-
-
-   </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/logIn" element={<LogIn />} />
+      <Route path="/createAccount" element={<CreateAccount />} />
+          <Route path="what_about" element={<LogIn />} />
+          <Route path="/about_us" element={<CreateAccount />} />
+          <Route path="/organization" element={<CreateAccount />} />
+          <Route path="/contact" element={<CreateAccount />} />
+          <Route path="/give_things" element={<CreateAccount />} />
+          <Route path="/organize_action" element={<CreateAccount />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
