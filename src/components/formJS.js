2bf60f78acba62ca98346f7b5725066fe0bee012
unknown <karol.nowakowski@poczta.onet.pl>
2022-07-08 18:48:43 +0200

    componentsFix

--- src/components/formJS.js
+++ src/components/formJS.js
@@ -1,29 +1,99 @@
 import React from "react";
 import "../styles/formJS.scss";
 
+const firstState = {
+    name:"",
+    email:"",
+    text:"",
+    nameError:"",
+    emailError:"",
+    textError:"",
+}
+export default class FormJs extends React.Component{
 
-class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    state = firstState;
+
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState(
+            {
+                [event.target.name]: isCheckbox
+                ?event.target.checked:
+                    event.target.value
+            })};
+    validate = () => {
+     let nameError = "";
+     let emailError = "";
+     let textError= "";
+
+     if(!this.state.name) {
+         nameError = "Wpisz imię!"
+     }
+
+     if (this.state.email.includes('@')){
+         emailError = 'Podany email nie jest prawidłowy!';
+     }
+     if (emailError || nameError || textError){
+         this.setState({emailError, nameError, textError});
+         return false;
+     }
+
+     return true;
     }
-
-    handleChange(event) {    this.setState({value: event.target.value});  }
-    handleSubmit(event) {
-        alert('Podano następujące imię: ' + this.state.value);
+    handleSubmit = event => {
         event.preventDefault();
-    }
+        const isValid = this.validate();
+        if(isValid){
+        console.log(this.state);
+        this.setState(firstState)
+        }};
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>        <label>
-                Imię:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
-                <input type="submit" value="Wyślij" />
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <section style={{display:'flex', flexDirection:'row'}}>
+                <div>
+                    <p>Wpisz swoje imię</p>
+                    <input
+                    name="Imię"
+                    type="text"
+                    placeholder="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    />
+                </div>
+                <div>{this.state.nameError}</div>
+                <div>
+                    <p>Wpisz swój mail</p>
+                    <input
+                        name="email"
+                        placeholder="email@mail.com"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                </section>
+                <div>
+                    <p>Wpisz swoją wiadomość</p>
+                    <input
+                        type="text"
+                        placeholder="Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit.
+                        Amet, aperiam aut beatae consequatur
+                        eum expedita facere inventore iste libero
+                        nesciunt, qui quos repellendus sed suscipit totam
+                        vel velit veritatis voluptas."
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Wyślij!</button>
+                </div>
             </form>
-        );
+        )
     }
 }
-export default NameForm;
+
+
--- src/components/main.js
+++ src/components/main.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {Header} from "./Header";
+import {Yellow} from "./YellowPart";
+import {FourSteps} from "./FourSteps";
+import {AboutUs} from "./AboutUs";
 import "../styles/main.scss";
-import {Header} from "./header";
-import {Yellow} from "./yellow_part";
-import {FourSteps} from "./four_steps";
-import {AboutUs} from "./about_us";
 
-
-
-
-
-export function Main(){
-    return(
+export function Main() {
+    return (
         <>
-           <section className="main_with_photo">
-
-            <section className="two_sections_main">
-
-   <section className="section_param">
-       <Header/>
-       <div className="div_dupa">
-            <p className="text_start_to_help">Zacznij pomagać!</p>
-            <p className="text_give_things">Oddaj niechciane rzeczy w zaufane ręce</p>
-           <img className="decoration" src="https://firebasestorage.googleapis.com/v0/b/test-space-d850c.appspot.com/o/Decoration.svg?alt=media&token=1c2c8f64-bc39-4175-a2c8-99a82911b539"/>
-           <div className="div_with_links">
-           <div className="link_give_it" >
-                <Link className="link_to_things" style={{textDecoration:'none'}}
-                      to="/give_things">ODDAJ RZECZY</Link>
-            </div>
-
-            <div className="link_organizing">
-                <Link className="link_to_organize" style={{textDecoration:'none'}}
-                      to="/organize_action">ZORGANIZUJ ZBIÓRKĘ</Link>
-
-            </div>
-           </div>
-       </div>
-
-   </section>
-
+            <section className="main_with_photo">
+                <section className="two_sections_main">
+                    <section className="section_param">
+                        <Header/>
+                        <div className="div_dupa">
+                            <p className="text_start_to_help">Zacznij pomagać!</p>
+                            <p className="text_give_things">Oddaj niechciane rzeczy w zaufane ręce</p>
+                            <img className="decoration"
+                                 src="https://firebasestorage.googleapis.com/v0/b/test-space-d850c.appspot.com/o/Decoration.svg?alt=media&token=1c2c8f64-bc39-4175-a2c8-99a82911b539"/>
+                            <div className="div_with_links">
+                                <div className="link_give_it">
+                                    <Link className="link_to_things" style={{textDecoration: 'none'}}
+                                          to="/give_things">ODDAJ RZECZY</Link>
+                                </div>
+                                <div className="link_organizing">
+                                    <Link
+                                        className="link_to_organize"
+                                        style={{textDecoration: 'none'}}
+                                        to="/organize_action"
+                                    >
+                                        ZORGANIZUJ ZBIÓRKĘ
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </section>
             </section>
-           </section>
             <Yellow/>
             <FourSteps/>
             <AboutUs/>
+
         </>
     )
 
